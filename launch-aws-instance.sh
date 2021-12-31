ebe61fbeb581d2c2485dcf488363ac1f1271daa0
VH-amit <amitt.gla@gmail.com>
2021-12-31 09:08:37 +0530

    created a new script file outside main folder

--- launch-aws-instance.sh
+++ launch-aws-instance.sh
@@ -3,4 +3,4 @@
 #1. create a AWS SPOT instance
 #2.Take that instance IP and register in DNS
 
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instance-request,Tags=[{Key=name,Value=string},{Key=frontend}]"
+aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=name,Value=string},{Key=frontend}]"
