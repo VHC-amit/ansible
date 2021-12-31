e9fbf0348171ff6a89e94008965c849c6b4096d8
VH-amit <amitt.gla@gmail.com>
2021-12-31 10:25:13 +0530

    created a new script file outside main folder

--- launch-aws-instance.sh
+++ launch-aws-instance.sh
@@ -6,7 +6,8 @@
 # Way to initiate an instance
 #aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=name,Value=frontend}]"
 
-# Way to initiate an instance through template
-TEMP_ID=lt-09abfdf2d74f1035b
+# Way to initiate an instance through template (template already crated in aws)
+
+TEMP_ID="lt-09abfdf2d74f1035b"
 TEMP_VER=1
-aws ec2 run-instances --launch-template LaunchTemplateID=${TEMP_ID}Version=${TEMP_VER}
+aws ec2 run-instances --launch-template LaunchTemplateID=${TEMP_ID},Version=${TEMP_VER}
