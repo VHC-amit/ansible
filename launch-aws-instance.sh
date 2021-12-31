aa7e4a3f7aad1de8de0ac7b3287adde6cf9f85a4
VHC-amit <95892336+VHC-amit@users.noreply.github.com>
2021-12-31 10:19:17 +0530

    Update launch-aws-instance.sh

--- launch-aws-instance.sh
+++ launch-aws-instance.sh
@@ -4,9 +4,9 @@
 #2.Take that instance IP and register in DNS
 
 # Way to initiate an instance
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=name,Value=frontend}]"
+#aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=name,Value=frontend}]"
 
 # Way to initiate an instance through template
-TEMP_ID=
-VEMP_VER=1
-aws ec2 run-instances --launch-template LaunchTemplateID=${TEMP_ID}Version=
+TEMP_ID=lt-09abfdf2d74f1035b
+TEMP_VER=1
+aws ec2 run-instances --launch-template LaunchTemplateID=${TEMP_ID}Version=${TEMP_VER}
