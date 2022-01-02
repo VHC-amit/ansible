d205ed9145b71d09e9cd52bb47024b4b2a78b89c
VHC-amit <95892336+VHC-amit@users.noreply.github.com>
2022-01-02 15:44:46 +0530

    Update launch-aws-instance.sh

--- launch-aws-instance.sh
+++ launch-aws-instance.sh
@@ -6,11 +6,19 @@
 # Way to initiate an instance
 #aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=name,Value=frontend}]"
 
-# Way to initiate an instance through template (template already crated in aws)
+# validate whether the instance is already up and running 
+aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | sed 's/"//g" | grep  -E 'running|stopped' &>/dev/null
+If [$? -eq -0]; then
+  echo "Instance is already there"
+  exit
+else
+  echo "Instance is not running, starting instance automatically"
+fi
 
+
+# Way to initiate an instance through template (template already crated in aws)
 TEMP_ID="lt-074a0c36ef35a9356"
 TEMP_VER=1
-#aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}
 aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}  --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" | jq
 
-# Good partice is to copy paste commands always to avoid typo
+
